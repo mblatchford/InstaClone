3ee89cc2f7ef6d1550299cb0f8051a70458a5afa
Michael Blatchford <mblatchford@mac.com>
2018-10-12 15:07:21 -0400

    basic photo layout complete, guts to be redone in JS

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const IMAGES = [
-    {img: assets/images/rm1.jpg alt:"The wife" },
+    {img: assets/images/rm1.jpg alt:"The Wife" },
     {img: assets/images/rm2.jpg alt:"Cafe"},
     {img: assets/images/rm3.jpg alt:"Strava Map" },
     {img: assets/images/rm4.jpg alt:"Cats" },
@@ -10,3 +10,19 @@ const IMAGES = [
     {img: assets/images/rm9.jpg alt:"CatPic 3029384827"}
 
 ]
+
+
+function createImg(pic) {
+    const newImage = document.createElement('img');
+    newImage.src = pic.img;
+    newImage.alt = pic.alt;
+    return newImage;
+}
+
+function createImgContainer() {
+    const imgContainer = document.createElement('div');
+    imgContainer.classList.add('imgContainer');
+    imgContainer.appendChild(createImg());
+
+    return buttonContainer;
+}
