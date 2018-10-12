9916f5ceca50240dba8f0e1791e4ceb5710558ab
Michael Blatchford <mblatchford@mac.com>
2018-10-12 13:55:01 -0400

    added image array and tags

---
+++ scripts/index.js
@@ -0,0 +1,12 @@
+const IMAGES = [
+    {img: assets/images/rm1.jpg alt:"The wife" },
+    {img: assets/images/rm2.jpg alt:"Cafe"},
+    {img: assets/images/rm3.jpg alt:"Strava Map" },
+    {img: assets/images/rm4.jpg alt:"Cats" },
+    {img: assets/images/rm5.jpg alt:"More Cats" },
+    {img: assets/images/rm6.jpg alt:"Still a Cat" },
+    {img: assets/images/rm7.jpg alt:"Do you only have cat pictures?" },
+    {img: assets/images/rm8.jpg alt:"Yep it's a cat" },
+    {img: assets/images/rm9.jpg alt:"CatPic 3029384827"}
+
+]
