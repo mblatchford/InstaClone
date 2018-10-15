3c77390a1dce68b65f1c804c4bc687bc59b3470f
Michael Blatchford <mblatchford@mac.com>
2018-10-15 14:03:47 -0400

    Working output, beginning modal transforms for gallery photo

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,53 @@
 const IMAGES = [
-    {img: assets/images/rm1.jpg alt:"The Wife" },
-    {img: assets/images/rm2.jpg alt:"Cafe"},
-    {img: assets/images/rm3.jpg alt:"Strava Map" },
-    {img: assets/images/rm4.jpg alt:"Cats" },
-    {img: assets/images/rm5.jpg alt:"More Cats" },
-    {img: assets/images/rm6.jpg alt:"Still a Cat" },
-    {img: assets/images/rm7.jpg alt:"Do you only have cat pictures?" },
-    {img: assets/images/rm8.jpg alt:"Yep it's a cat" },
-    {img: assets/images/rm9.jpg alt:"CatPic 3029384827"}
-
+    {src: "assets/images/rm1.jpg", alt:"The Wife" },
+    {src: "assets/images/rm2.jpg", alt:"Cafe"},
+    {src: "assets/images/rm3.jpg", alt:"Strava Map" },
+    {src: "assets/images/rm4.jpg", alt:"Cats" },
+    {src: "assets/images/rm5.jpg", alt:"More Cats" },
+    {src: "assets/images/rm6.jpg", alt:"Still a Cat" },
+    {src: "assets/images/rm7.jpg", alt:"Do you only have cat pictures?" },
+    {src: "assets/images/rm8.jpg", alt:"Yep it's a cat" },
+    {src: "assets/images/rm9.jpg", alt:"CatPic 3029384827"}
 ]
 
 
-function createImg(pic) {
+const thumbnailContainer = document.querySelector('[data-container]')
+const galleryElement = document.querySelector('[data-largeOutput]');
+
+
+function createImage(imageData) {
     const newImage = document.createElement('img');
-    newImage.src = pic.img;
-    newImage.alt = pic.alt;
+    
+    // theImage.src = imageURL;
+    newImage.src = imageData.src;
+    newImage.alt = imageData.alt;
+
+    // add an event listener to the image
+    newImage.addEventListener('click', function (event) {
+        console.log("*hello*")
+        // the element that got clicked is accessible
+        // as `event.target`
+        // And, I can read the `src` attribute!
+        console.log(`This is the event source ${event.target.src}`);
+        // I can now set the output image's src
+        // to event.target.src!
+        galleryElement.src =  event.target.src;
+    });
     return newImage;
 }
 
-function createImgContainer() {
+function createImgContainer(imageData) {
     const imgContainer = document.createElement('div');
-    imgContainer.classList.add('imgContainer');
-    imgContainer.appendChild(createImg());
+    imgContainer.classList.add('thumbnail-item');
+    const image = createImage(imageData);
+    imgContainer.appendChild(image);
 
-    return buttonContainer;
+    return imgContainer;
 }
+
+IMAGES.forEach(function (imageData) {
+    // We pass that image URL to our createThumbnail func
+    let testThumb = createImgContainer(imageData);
+    // then append that thumbnail to the page.
+    thumbnailContainer.appendChild(testThumb);
+});
